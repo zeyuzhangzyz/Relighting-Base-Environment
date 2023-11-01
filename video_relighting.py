cd26c2343ed6b26b2ab5ef48360d66d0ca628e18
lingxueyi <2321872699@qq.com>
2023-11-01 23:38:08 +0800

    updata

--- video_relighting.py
+++ video_relighting.py
@@ -8,7 +8,7 @@ def relighting_single_image():
     # Predict the segmentation result of the face
     face_name = 'face'
     env_name = 'bgi'
-    command = 'conda activate RBE_Paddle & python tools/predict.py        ' \
+    command = 'conda activate Paddle & python tools/predict.py        ' \
           '--config configs/human_pp_humansegv2_lite.yml      ' \
           '--model_path pretrained_models/human_pp_humansegv2_lite_192x192_pretrained/model.pdparams        ' \
           '--image_path img/'+face_name+'.jpg' \
